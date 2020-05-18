1c7ad465390a0902484410026c5ddfc6923a8347
Dongqin Zhou <dongqinzhou1110@gmail.com>
2020-05-18 18:49:14 -0400

    update summary memo

--- summary_multi_anon.py
+++ summary_multi_anon.py
@@ -21,7 +21,7 @@ NAN_LABEL = -1
 
 def parse_args():
     parser = argparse.ArgumentParser()
-    parser.add_argument("--memo", type=str, default='initial')
+    parser.add_argument("--memo", type=str, default='colight_original_reward_queue_length_state_lane_num_vehicle')
     parser.add_argument("-b", action="store_true",default=False, help="run baseline analysis") ##
     return parser.parse_args()
 
